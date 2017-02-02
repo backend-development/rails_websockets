fb642bed56b611f2a4f0bae1c0ea61cddf863412
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-02-02 12:06:09 +0100

    check for not started

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-  def user_avatar user
+  def user_at_step step  
+    user = step.user
     image = if user.image.present?
       image_tag user.image, alt: user.full_name
     else
