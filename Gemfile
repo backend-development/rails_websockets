c6ece3fb584099923d8a36e2973fced298921530
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-02-02 14:12:14 +0100

    svg logo and layout fixes

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'inline_svg'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
-  def user_at_step step  
-    user = step.user
+  def user_avatar user
     image = if user.image.present?
       image_tag user.image, alt: user.full_name
     else
       image_tag 'default.png', alt: user.full_name
     end
-
     content_tag "span", image
+  end
+
+  def user_at_step step
+    user = step.user
+    user_avatar user
   end
 end
